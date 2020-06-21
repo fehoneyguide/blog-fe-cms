c6bfbea320f42d535cd841e53474c05bc2f9b226
yayxs <leaderywl@163.com>
2020-06-21 22:14:09 +0800

    feat(add env config): add env config
    add env config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect } from "react";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    let BASE_URL;
+    if (process.env.NODE_ENV === "development") {
+      BASE_URL = `dev/api/v1`;
+    } else if (process.env.NODE_ENV === "production") {
+      BASE_URL = `prod/api/v1`;
+    }
+    console.log(BASE_URL);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        react-typescript 探索
+        {/* {process.env.MY_NAME} */}
+        {}
+        {process.env.REACT_APP_INFO}
+        <hr />
+        {process.env.REACT_APP_BASE_URL}
+
+        <hr />
       </header>
     </div>
   );
