bec7e1e7f3ac18e54bb8a00c4c97ee335f00098d
yayxs <leaderywl@163.com>
2020-06-26 09:19:41 +0800

    feat(add posts demo page): add posts demo page
    add posts demo page

---
+++ src/pages/Posts.tsx
@@ -0,0 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-22 19:43:10
+ * @LastEditTime: 2020-06-22 19:50:03
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \blog-fe-cms\src\pages\Posts.tsx
+ */
+
+import React, { ReactElement } from "react";
+
+interface Props {}
+
+export default function Posts({}: Props): ReactElement {
+  return <div></div>;
+}
